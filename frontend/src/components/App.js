d14fbcb46000a57b0d68047ab2fb9629c2cdabf2
margarita-tsaruk <margarita.tsaruk@yandex.ru>
2022-09-26 11:38:33 +0200

    Fix: project 15 - fixing likes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -164,8 +164,9 @@ function App() {
   }
 
   function handleCardLike(card) {
+    console.log(card)
     const isLiked = card.likes.some(i => i === currentUser._id);
-  
+    console.log(isLiked)
     api.changeLikeCardStatus(card._id, isLiked) 
       .then((newCard) => {
         setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
